72891e4df5032a4145ed2d730465fe75db4c2975
Johann Jacobsohn <j.jacobsohn@satzmedia.de>
2013-10-27 01:18:52 +0200

    cleanup

--- source/parser/uke/index.js
+++ source/parser/uke/index.js
@@ -1,8 +1,3 @@
-//~
-//~
-//~ Target: 150 Lines
-//~ @TODO: split and rejoin "süßer Abschluss" and "Beilagen & Gemüse" by ,
-//~ 
 "use strict";
 var exec = require('child_process').exec;
 
@@ -10,79 +5,20 @@ function trim(item){
 	return item.replace(/[\s]+/g, " ").trim();
 }
 
-function parser(file, mensaId, week, callback){
-	var names = [];
-	exec('pdftotext -q -layout ' + file + " - ", function (error, stdout, stderr){
-		var s = stdout.split("\n"), menu = [], rows = [];
-
-		var header;
-
-		// remove header
-		for(var i = 0; i<s.length; i++){
-			if( s[i].search(/Montag.*Dienstag/) !== -1 ){
-				header = s[i];
-				s[i] = null;
-				break;
-			}
-			s[i] = null;
-		}
-		s = s.filter(function(item){
-			return item != null;
-		});
-
-		// remove footer
-		var footer = s.splice(-3,2).join("");
-		var additivesList = extractAdditives(footer);
-
-		// split columns
-		var horBounds = getBoundaries(header);
-		//~ console.log(header)
-		var s = getColums(s, horBounds);
-
-		// first column == names; tread separately
-		var names = s.shift().map(trim).join("\n").split(/[\n]{3,}/g).map(trim);
-		var veggi = names.indexOf("Vegetarisch");
-		if(veggi == -1){
-			veggi = names.indexOf("vegetarisch");
-		}
-		names.splice(veggi, 1);
-		names[ veggi - 1 ] += " - Vegetarisch";
-		//~ console.log(names)
-
-		// last column == additional information; ignore.
-		//~ s.pop();
-//~ console.log(s[4])
-		// split rows
-		for(var x = 0; x < s.length; x++){
-			rows = splitRows(s[x], names);
-			for(var y = 0; y < rows.length; y++){
-				menu.push( parse(rows[y], x, names[y] || "", week, additivesList) );
-			}
-		}
-
-		callback(null, menu);
-	});
-}
-
-function extractAdditives(str){
-	var v = [],
-		re = /[0-9]+ ([^,]*),/g,
-		match;
-	while (match = re.exec(str)) {
+function findAll(str, regexp){
+	var v = [], match;
+	while(!!(match = regexp.exec(str))) {
 		v.push(match[1]);
 	}
 	return v;
 }
 
+function extractAdditives(str){
+	return findAll(str, /[0-9]+ ([^,]*),/g);
+}
 
 function findAdditives(str){
-	var v = [],
-		re = /\(([0-9]+)\),/g,
-		match;
-	while (match = re.exec(str)) {
-		v.push(parseInt(match[1], 10));
-	}
-	return v;
+	return findAll(str, /\(([0-9]+)\),/g);
 }
 
 //~ There are three markers which mark vertical boundaries:
@@ -104,16 +40,11 @@ function splitRows(column, types){
 		})
 	;
 
-	var bug = types.indexOf("Beilagen & Gemüse");
-	//~ console.log(found, types.indexOf("Beilagen & Gemüse"), found[bug] + 3 )
-
 	// Beilagen & Gemüse ist genau drei Zeilen lang
+	var bug = types.indexOf("Beilagen & Gemüse");
 	found.push(found[bug-1] + 3);
-
 	found.push(column.length);
-
 	found = found.sort(function(a,b){return a - b; });
-	//~ console.log(found)
 
 	found = found
 		.map(function(item, i, arr){
@@ -125,37 +56,6 @@ function splitRows(column, types){
 	return found;
 }
 
-//~ function getBoundaries(s){
-	//~ var str = s.map(function(line){
-		//~ var a = line.split("").map(function(char){
-			//~ return +(char !== " " && !!char);
-		//~ })
-		//~ return a
-	//~ })
-	//~ .reduce(function(prev, line){
-		//~ var a = (prev.length > line.length ? prev : line);
-//~ 
-		//~ a = a.map(function(char, i){
-			//~ return !!line[i] + Math.min(prev[i] || 0, 8);
-		//~ });
-		//~ return a;
-	//~ })
-	//~ // einmal ist keinmal
-	//~ .map(function(i){
-		//~ return i < 3 ? 0 : i;
-	//~ })
-	//~ .join("");
-//~ 
-	//~ var v = [],
-		//~ re = /0{2,}/g,
-		//~ match;
-	//~ while (match = re.exec(str)) {
-		//~ console.log(match.index, Math.floor(match[0].length/2))
-		//~ v.push(match.index + Math.floor(match[0].length/2));
-	//~ }
-	//~ return v;
-//~ }
-
 // -------------#####--------#####--------#####-------#####--------######-------------------
 //      [^]             ^             ^           ^            ^              [^]
 function getBoundaries(line){
@@ -164,19 +64,13 @@ function getBoundaries(line){
 
 	markers = markers.map(function(mark){
 		var re = new RegExp(mark + "([\\s]+)");
-		//~ console.log(re, line)
 		var match = re.exec(line);
 		return match.index + Math.floor( mark.length + match[1].length/2 );
-		//~ return Math.floor( mark.length + match[1].length/2 );
 	});
 
-	//~ var average = markers.reduce(function(a, b){ return a + b; })/markers.length;
-
 	var meanDistance = (markers[markers.length-1] - markers[0])/(markers.length-1);
-//~ console.log(meanDistance)
 	markers.push(Math.floor(markers[markers.length-1] + meanDistance));
 	markers.unshift(Math.floor(markers[0] - meanDistance));
-//~ console.log(markers)
 	return markers;
 }
 
@@ -191,35 +85,26 @@ function getColums(s, v){
 		v = JSON.parse(orig_v);
 
 		for(var i = 0; i<c.length; i++){
-			//~ console.log(line[v[i]])
 			if(line && line[v[i]] && line[v[i]] !== " "){
-
 
 				// first, look for unusual long whitespace within current string
 				var whitemarker = line.substring(v[i-1] || 0, v[i]).match(/[^\s][\s]{2,}/);
-				//~ console.log(whitemarker)
 
 				if(whitemarker){
 					x = v[i];
 					v[i] = v[i-1] + whitemarker.index + 1;
-					//~ console.log("set to", x, v[i-1], whitemarker.index, v[i], line.substring(v[i-1] || 0, v[i]), line.substring(v[i-1] || 0, x) )
 				} else {
-					//~ v[i] = v[i]-1;
 					x = left = right = v[i];
-					//~ console.log(line[v[i]])
 					while(true){
 						left--;
 						right++;
 						if(!left || !line[left] || line[left] === " "){
 							v[i] = left;
-							//~ console.log("left resolved at: ...", ""+line[left]+line[left+1]+line[left+2]+line[left+3], line.substring(v[i-1] || 0, v[i]).trim(), line[x] )
 							break;
 						} else if(!line[right] || line[right] === " "){
 							v[i] = right;
-							//~ console.log("right resolved at: ...", ""+line[right-3]+line[right-2]+line[right-1]+line[right], line.substring(v[i-1] || 0, v[i]).trim(), line[x])
 							break;
 						}
-						//~ console.log("line not complete:", line[left], line[right], left, right)
 					}
 				}
 			}
@@ -261,13 +146,61 @@ function parse(entry, x, y, week, additivesList){
 		normalPrice : price,
 		name        : dish,
 		mensaId     : "uke",
-		week        : week,
 		type        : y,
 		date        : dateString,
 		kcal        : kcal || null,
 		properties  : [],
 		additives   : additives
 	};
+}
+
+
+function parser(file, mensaId, week, callback){
+	var names = [];
+	exec('pdftotext -q -layout ' + file + " - ", function (error, stdout, stderr){
+		var s = stdout.split("\n"), menu = [], rows = [], header;
+
+		// remove header
+		for(var i = 0; i<s.length; i++){
+			if( s[i].search(/Montag.*Dienstag/) !== -1 ){
+				header = s[i];
+				s[i] = null;
+				break;
+			}
+			s[i] = null;
+		}
+		s = s.filter(function(item){
+			return item;
+		});
+
+		// remove footer
+		var footer = s.splice(-3,2).join("");
+		var additivesList = extractAdditives(footer);
+
+		// split columns
+		var horBounds = getBoundaries(header);
+		var cols = getColums(s, horBounds);
+
+		// first column == names; tread separately
+		var names = cols.shift().map(trim).join("\n").split(/[\n]{3,}/g).map(trim);
+		var veggi = names.indexOf("Vegetarisch");
+		if(veggi === -1){
+			veggi = names.indexOf("vegetarisch");
+		}
+		names.splice(veggi, 1);
+		names[ veggi - 1 ] += " - Vegetarisch";
+
+		// last column == additional information; ignore.
+		// split rows
+		for(var x = 0; x < cols.length; x++){
+			rows = splitRows(cols[x], names);
+			for(var y = 0; y < rows.length; y++){
+				menu.push( parse(rows[y], x, names[y] || "", week, additivesList) );
+			}
+		}
+
+		callback(null, menu);
+	});
 }
 
 exports.parser = parser;
