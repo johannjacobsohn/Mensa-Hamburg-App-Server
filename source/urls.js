3c890c694a08b49ca297cca0c82fe22b1062394d
Johann Jacobsohn <j.jacobsohn@satzmedia.de>
2014-01-05 23:20:07 +0100

    fix year handling

--- source/urls.js
+++ source/urls.js
@@ -1,109 +1,110 @@
-/** 
+/**
  * Static conf file for urls to load data from
 */
 "use strict";
 var baselink = "http://speiseplan.studierendenwerk-hamburg.de/index.php/de/";
+var year = new Date().getFullYear();
 var mensen = [
 	{
 		name: "Alexanderstrasse",
 		id: "alexanderstrasse",
-		url: baselink + "660/2013/{{week}}/",
+		url: baselink + "660/" + year + "/{{week}}/",
 		parser: "studhh"
 	},
 	{
 		name: "Armgartstrasse",
 		id: "armgartstrasse",
-		url: baselink + "590/2013/{{week}}/",
+		url: baselink + "590/" + year + "/{{week}}/",
 		parser: "studhh"
 	},
 	{
 		name: "Averhoffstrasse",
 		id: "averhoffstrasse",
-		url: baselink + "650/2013/{{week}}/",
+		url: baselink + "650/" + year + "/{{week}}/",
 		parser: "studhh"
 	},
 	{
 		name: "Bergedorf",
 		id: "bergedorf",
-		url: baselink + "520/2013/{{week}}/",
+		url: baselink + "520/" + year + "/{{week}}/",
 		parser: "studhh"
 	},
 	{
 		name: "Berliner Tor",
 		id: "berlinertor",
-		url: baselink + "530/2013/{{week}}/",
+		url: baselink + "530/" + year + "/{{week}}/",
 		parser: "studhh"
 	},
 	{
 		name: "Botanischer Garten",
 		id: "botanischergarten",
-		url: baselink + "560/2013/{{week}}/",
+		url: baselink + "560/" + year + "/{{week}}/",
 		parser: "studhh"
 	},
 	{
 		name: "Bucerius Law School",
 		id: "buceriuslawschool",
-		url: baselink + "410/2013/{{week}}/",
+		url: baselink + "410/" + year + "/{{week}}/",
 		parser: "studhh"
 	},
 	{
 		name: "Campus",
 		id: "campus",
-		url: baselink + "340/2013/{{week}}/",
+		url: baselink + "340/" + year + "/{{week}}/",
 		parser: "studhh"
 	},
 	{
 		name: "City Nord",
 		id: "citynord",
-		url: baselink + "550/2013/{{week}}/",
+		url: baselink + "550/" + year + "/{{week}}/",
 		parser: "studhh"
 	},
 	{
 		name: "Finkenau",
 		id: "finkenau",
-		url: baselink + "620/2013/{{week}}/",
+		url: baselink + "620/" + year + "/{{week}}/",
 		parser: "studhh"
 	},
 	{
 		name: "Geomatikum",
 		id: "geomatikum",
-		url: baselink + "540/2013/{{week}}/",
+		url: baselink + "540/" + year + "/{{week}}/",
 		parser: "studhh"
 	},
 	{
 		name: "Harburg",
 		id: "harburg",
-		url: baselink + "570/2013/{{week}}/",
+		url: baselink + "570/" + year + "/{{week}}/",
 		parser: "studhh"
 	},
 	{
 		name: "Jungiusstrasse",
 		id: "jungiusstrasse",
-		url: baselink + "610/2013/{{week}}/",
+		url: baselink + "610/" + year + "/{{week}}/",
 		parser: "studhh"
 	},
 	{
 		name: "Philosophenturm",
 		id: "philosophenturm",
-		url: baselink + "350/2013/{{week}}/",
+		url: baselink + "350/" + year + "/{{week}}/",
 		parser: "studhh"
 	},
 	{
 		name: "Stellingen",
 		id: "stellingen",
-		url: baselink + "580/2013/{{week}}/",
+		url: baselink + "580/" + year + "/{{week}}/",
 		parser: "studhh"
 	},
 	{
 		name: "Studierendenhaus",
 		id: "studierendenhaus",
-		url: baselink + "310/2013/{{week}}/",
+		url: baselink + "310/" + year + "/{{week}}/",
 		parser: "studhh"
 	},
 	{
 		name: "Cafe CFEL",
 		id: "cafecfel",
-		url: baselink + "680/2013/{{week}}/",
+		url: baselink + "680/" + year + "/{{week}}/",
 		parser: "studhh"
 	},
 	{
@@ -115,7 +116,7 @@ var mensen = [
 	{
 		name: "empty mensa - just for testing",
 		id: "emptymensa",
-		url: baselink + "empty/2013/{{week}}/",
+		url: baselink + "empty/" + year + "/{{week}}/",
 		parser: "studhh"
 	}
 ];
