7e74783937079e7e29d6cdb5f88943b109e9bf07
Johann Jacobsohn <j.jacobsohn@satzmedia.de>
2016-08-08 19:10:56 +0200

    update getWeek function

--- source/getweek.js
+++ source/getweek.js
@@ -1,15 +1,17 @@
 "use strict";
+// This script is released to the public domain and may be used, modified and
+// distributed without restrictions. Attribution not necessary but appreciated.
+// Source: http://weeknumber.net/how-to/javascript
 
-// http://syn.ac/tech/19/get-the-weeknumber-with-javascript/
 module.exports = function(date) {
 	date = date || new Date();
-	var determinedate = new Date();
-	determinedate.setFullYear(date.getFullYear(), date.getMonth(), date.getDate());
-	var D = determinedate.getDay();
-	if(D === 0){ D = 7; }
-	determinedate.setDate(determinedate.getDate() + (4 - D));
-	var YN = determinedate.getFullYear();
-	var ZBDoCY = Math.floor((determinedate.getTime() - new Date(YN, 0, 1, -6)) / 86400000);
-	var WN = 1 + Math.floor(ZBDoCY / 7);
-	return WN;
+
+	date.setHours(0, 0, 0, 0);
+	// Thursday in current week decides the year.
+	date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+	// January 4 is always in week 1.
+	var week1 = new Date(date.getFullYear(), 0, 4);
+	// Adjust to Thursday in week 1 and count number of weeks from date to week1.
+	return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000 - 3 + (week1.getDay() + 6) % 7) / 7);
 };
+
